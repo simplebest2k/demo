4bfde8b2b313d3cf49c02b1db0cd213907a11a9c
simplebest2k <simplebest2k@naver.com>
2021-10-27 15:44:35 +0900

    event2, event3 함수추가

--- src/main/java/com/example/test1/EventController.java
+++ src/main/java/com/example/test1/EventController.java
@@ -1,7 +1,17 @@
 package com.example.test1;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
+    @GetMapping("event2")
+    public String event2() {
+        return "event2";
+    }
+
+    @GetMapping("event3")
+    public String event3() {
+        return "event3";
+    }
 }
