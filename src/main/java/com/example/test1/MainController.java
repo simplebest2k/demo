084540b7e75ea2a01ab314006616ef7b00f4b21e
simplebest2k <simplebest2k@naver.com>
2021-10-27 15:33:20 +0900

    main1 함수추가

--- src/main/java/com/example/test1/MainController.java
+++ src/main/java/com/example/test1/MainController.java
@@ -1,7 +1,12 @@
 package com.example.test1;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @GetMapping("/main1")
+    public String main1() {
+        return "main1";
+    }
 }
