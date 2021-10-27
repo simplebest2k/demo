446ba45eeb8486eaf2e97e44b9f562639687305c
simplebest2k <simplebest2k@naver.com>
2021-10-27 15:27:57 +0900

    test1 함수추가

--- src/main/java/com/example/test1/TestController.java
+++ src/main/java/com/example/test1/TestController.java
@@ -1,8 +1,12 @@
 package com.example.test1;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    @GetMapping("/test1")
+    public String test1() {
+        return "test1";
+    }
 }
